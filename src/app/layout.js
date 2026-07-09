2e267eef15646fcb2f42fa9ad12ee93f652f073c
kingsleyonyekwelu <kimonyekwelu@gmail.com>
2026-07-09 15:44:08 +0100

    tawto

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import { Geist } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const geist = Geist({ subsets: ["latin"], variable: "--font-geist-sans" });
@@ -12,7 +13,27 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${geist.variable} h-full`}>
-      <body className="min-h-full antialiased bg-gtb-dark text-white">{children}</body>
+      <body className="min-h-full antialiased bg-gtb-dark text-white">
+        {children}
+
+        {process.env.NEXT_PUBLIC_TAWKTO_PROPERTY_ID && (
+          <Script id="tawkto-widget" strategy="afterInteractive">
+            {`
+              var Tawk_API = Tawk_API || {};
+              var Tawk_LoadStart = new Date();
+              (function () {
+                var s1 = document.createElement("script");
+                var s0 = document.getElementsByTagName("script")[0];
+                s1.async = true;
+                s1.src = 'https://embed.tawk.to/${process.env.NEXT_PUBLIC_TAWKTO_PROPERTY_ID}/${process.env.NEXT_PUBLIC_TAWKTO_WIDGET_ID}';
+                s1.charset = 'UTF-8';
+                s1.setAttribute('crossorigin', '*');
+                s0.parentNode.insertBefore(s1, s0);
+              })();
+            `}
+          </Script>
+        )}
+      </body>
     </html>
   );
 }
